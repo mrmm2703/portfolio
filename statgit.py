50799411a3b553eee14af92c62167b8219cfe994
mrmm2703 <35401592+mrmm2703@users.noreply.github.com>
2020-06-28 18:02:26 +0100

    Removed sensitive info

--- statgit.py
+++ statgit.py
@@ -5,56 +5,72 @@ import time
 import requests
 from requests.auth import HTTPBasicAuth
 from api_key import api_key
+from api_key import git_username
+from api_key import git_pass
+from api_key import git_key
+from datetime import datetime
 
 coding_tag_id = "7895193"
 
-base_url = "https://github.com/mrmm2703/"
+base_url = "https://" + git_username + ":" + git_key + "@github.com/mrmm2703/"
 
-repos = [
-    "megagon",
-    "spotify-party",
-    "mental-screen",
-    "pepper-project",
-    "portfolio",
-    "spotify-party-mobile"
-]
+repos = []
 
-while True:
-    times = []
-    data = {"repos": {}}
-    total_lines = 0
+with open("repolist.txt", "r") as repolistfile:
+    reader = csv.reader(repolistfile)
+    for line in reader:
+        repos = line
 
-    # Weekly coding time from Toggl
-    response = requests.get("https://www.toggl.com/reports/api/v2/weekly?workspace_id=4274327&user_agent=portfolio&tag_ids="+coding_tag_id, auth=HTTPBasicAuth(api_key, "api_token"))
+
+times = []
+data = {"repos": {}}
+total_lines = 0
+
+# Weekly coding time from Toggl
+response = requests.get("https://www.toggl.com/reports/api/v2/weekly?workspace_id=4274327&user_agent=portfolio&tag_ids="+coding_tag_id, auth=HTTPBasicAuth(api_key, "api_token"))
+js = response.json()
+data["coding_time_week"] = js["total_grand"]
+
+json_file = open("stats.json", "r")
+json_data = json.load(json_file)
+json_file.close()
+
+# Repo loop
+for repo in repos:
+    name = repo
+    # Get last update from GitHub
+    response = requests.get("https://api.github.com/repos/mrmm2703/"+name, auth=HTTPBasicAuth(git_username, git_pass))
     js = response.json()
-    data["coding_time_week"] = js["total_grand"]
+    last_edit = js["updated_at"]
+    datetime_obj = datetime.strptime(last_edit, "%Y-%m-%dT%H:%M:%SZ")
+    last_edit_ts = datetime_obj.timestamp()
+    if name in json_data["repos"]:
+        if not (json_data["repos"][name]["last_edit"] == last_edit_ts): # Update detected
+            cont2 = True
+        else: # No update
+            cont2 = False
+    else: # Update detected
+        cont2 = True
 
-    # Repo loop
-    for repo in repos:
-        name = repo
+    if not cont2:
+        print(name + " already exists. Skipping...")
+        data["repos"][repo] = {
+            "name": json_data["repos"][repo]["name"],
+            "last_edit": last_edit_ts,
+            "creation_date": json_data["repos"][repo]["creation_date"],
+            "lang": json_data["repos"][repo]["lang"]
+        }
+        times.append([last_edit_ts, repo])
+        total_lines = total_lines + int(json_data["repos"][repo]["lang"]["SUM"])
+
+    if (cont2):
+        print(name + " doesn't already exist. Proceeding...")
         # Download and count repo lines
         os.system("git clone " + base_url + repo + ".git")
         os.system("cloc --csv " + repo + " > " + repo + ".csv")
         lang = {}
 
-        # Get relative time with git
-        os.system("git --git-dir " + repo + "/.git log --pretty=\"%cr\" > " + repo + ".txt")
-        with open(repo + ".txt", "r") as file:
-            reader = csv.reader(file)
-            rows = list(reader)
-            total_rows = len(rows)
-        i = 0
-        with open(repo + ".txt", "r") as file1:
-            reader1 = csv.reader(file1)
-            for row in reader1:
-                if i == 0:
-                    last_edit = row[0]
-                if i == total_rows-1:
-                    creation_date = row[0]
-                i += 1
-        os.system("del " + repo + ".txt")
-        
-        # Get UNIX timestamp from git
+        # Get UNIX timestamp with git
         os.system("git --git-dir " + repo + "/.git log --pretty=\"%ct\" > " + repo + ".txt")
         with open(repo + ".txt", "r") as file:
             reader = csv.reader(file)
@@ -65,7 +81,10 @@ while True:
             reader1 = csv.reader(file1)
             for row in reader1:
                 if i == 0:
-                    times.append([int(row[0]), repo])
+                    last_edit = last_edit_ts
+                    times.append([last_edit_ts, repo])
+                if i == total_rows-1:
+                    creation_date = row[0]
                 i += 1
         os.system("del " + repo + ".txt")
 
@@ -90,32 +109,19 @@ while True:
         os.system("rmdir /Q /S " + repo)
 
         # Construct JSON
-        print("---------")
-        print(name)
-        print(last_edit)
-        print(creation_date)
-        print(lang)
-        print("---------")
         data["repos"][repo] = {
             "name": name,
             "last_edit": last_edit,
             "creation_date": creation_date,
             "lang": lang
         }
-        data["total_lines"] = total_lines
-    
-    # Get the last project
-    times = sorted(times, key=lambda x: x[0], reverse=True)
-    last_project = times[0][1]
-    data["last_project"] = last_project
+    data["total_lines"] = total_lines
 
-    # Write JSON
-    with open("stats.json", "w") as file2:
-        json.dump(data, file2)
-    
-    # Finish off and wait
-    print(data)
-    print("------------------------------------")
-    print("               WAIT")
-    print("------------------------------------")
-    time.sleep(30*60)
+# Get the last project
+times = sorted(times, key=lambda x: x[0], reverse=True)
+last_project = times[0][1]
+data["last_project"] = last_project
+
+# Write JSON
+with open("stats.json", "w") as file2:
+    json.dump(data, file2)
