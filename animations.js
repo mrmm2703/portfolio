b6a224c8f4d52e564bac98c96928bc919aba2b7b
Muhammad Rahman <mrmm2703@gmail.com>
2021-04-21 08:45:12 +0000

    Updated animations

--- animations.js
+++ animations.js
@@ -1,29 +1,43 @@
 // Hover handlers
 $(".img").mouseover(function () {
     $(this).stop()
-    $(this).animate({
-        width: "95%"
-    }, {
-        step: function(now, fx) {
-            // console.log(fx.pos)
-            // console.log('box-shadow: 0 2px 2px 0 rgba(0, 0, 0, '+0.14*(fx.pos+1)+'), 0 3px 1px -2px rgba(0, 0, 0, '+0.2*(fx.pos+1)+'), 0 1px 5px 0 rgba(0, 0, 0, '+0.12*(fx.pos+1)+')')
-            $(this).css('height', $(this).width()+"px")
-            $(this).css('margin-top', (origMargin - ($(this).height() - origSize))+"px")
-            // $(this).css('box-shadow', ('box-shadow: 0 2px 2px 0 rgba(0, 0, 0, '+0.14*(fx.pos+1)+'), 0 3px 1px -2px rgba(0, 0, 0, '+0.2*(fx.pos+1)+'), 0 1px 5px 0 rgba(0, 0, 0, '+0.12*(fx.pos+1)+')'))
-        }, duration: 150
-    })
+    if ($(this).hasClass("feature")) {
+        $(this).animate({
+            width: "95%",
+            height: "95%"
+        }, 150)
+    } else {
+        $(this).animate({
+            width: "95%"
+        }, {
+            step: function(now, fx) {
+                // console.log(fx.pos)
+                // console.log('box-shadow: 0 2px 2px 0 rgba(0, 0, 0, '+0.14*(fx.pos+1)+'), 0 3px 1px -2px rgba(0, 0, 0, '+0.2*(fx.pos+1)+'), 0 1px 5px 0 rgba(0, 0, 0, '+0.12*(fx.pos+1)+')')
+                $(this).css('height', $(this).width()+"px")
+                $(this).css('margin-top', (origMargin - ($(this).height() - origSize))+"px")
+                // $(this).css('box-shadow', ('box-shadow: 0 2px 2px 0 rgba(0, 0, 0, '+0.14*(fx.pos+1)+'), 0 3px 1px -2px rgba(0, 0, 0, '+0.2*(fx.pos+1)+'), 0 1px 5px 0 rgba(0, 0, 0, '+0.12*(fx.pos+1)+')'))
+            }, duration: 150
+        })
+    }
 })
 $(".img").mouseout(function () {
     $(this).stop()
-    $(this).animate({
-        width: "90%"
-    }, {
-        step: function(now, fx) {
-            $(this).css('height', $(this).width()+"px")
-            $(this).css('margin-top', (origMargin - ($(this).height() - origSize))+"px")
-            // $(this).css('box-shadow', 'box-shadow: 0 2px 2px 0 rgba(0, 0, 0, '+0.24*(1-fx.pos)+'), 0 3px 1px -2px rgba(0, 0, 0, '+0.4*(fx.pos+1)+'), 0 1px 5px 0 rgba(0, 0, 0, '+0.24*(fx.pos+1)+')')
-        }, duration: 150
-    })
+    if ($(this).hasClass("feature")) {
+        $(this).animate({
+            width: "90%",
+            height: "90%"
+        },150)
+    } else {
+        $(this).animate({
+            width: "90%"
+        }, {
+            step: function(now, fx) {
+                $(this).css('height', $(this).width()+"px")
+                $(this).css('margin-top', (origMargin - ($(this).height() - origSize))+"px")
+                // $(this).css('box-shadow', 'box-shadow: 0 2px 2px 0 rgba(0, 0, 0, '+0.24*(1-fx.pos)+'), 0 3px 1px -2px rgba(0, 0, 0, '+0.4*(fx.pos+1)+'), 0 1px 5px 0 rgba(0, 0, 0, '+0.24*(fx.pos+1)+')')
+            }, duration: 150
+        })
+    }
 })
 
 // Click handlers
